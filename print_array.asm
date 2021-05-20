e343cc10070d387f52fe81b466c472cefb15c04f
17521175 <84486385+17521175@users.noreply.github.com>
2021-05-20 10:39:48 +0700

    Update print_array.asm

--- print_array.asm
+++ print_array.asm
@@ -9,24 +9,24 @@ string3: .asciiz " "
 string4: .asciiz " \n"
 .text
 	li $t1, 0               #tạo biến đếm cho chuỗi 1
-	la $a1, array1     #lưu địa chỉ chuỗi 1    
-	lw $s1, size1      #lưu kích thước chuỗi 1
-	li $t2, 0                 #tạo biến đếm cho chuỗi 2
-	la $a2, array2       #lưu địa chỉ chuỗi 2
-	lw $s2, size2        #lưu kích thước chuỗi 2
+	la $a1, array1     	#lưu địa chỉ chuỗi 1    
+	lw $s1, size1      	#lưu kích thước chuỗi 1
+	li $t2, 0               #tạo biến đếm cho chuỗi 2
+	la $a2, array2       	#lưu địa chỉ chuỗi 2
+	lw $s2, size2        	#lưu kích thước chuỗi 2
 	la $a0, string1
 	li $v0, 4
 	syscall
 print_array1:
-	beq $t1, $s1, newline     #nếu $t1 = $s1 thì rẻ nhanh sang newline
-	lw $a0, ($a1)                  #đưa địa chỉ của $a1 vào $a0
-	li $v0, 1                          #xuất giá trị integer của $a0
+	beq $t1, $s1, newline     	#nếu $t1 = $s1 thì rẻ nhanh sang newline
+	lw $a0, ($a1)                  	#đưa địa chỉ của $a1 vào $a0
+	li $v0, 1                       #xuất giá trị integer của $a0
 	syscall
 	la $a0, string3
 	li $v0, 4
 	syscall
-	addi $a1, $a1, 4            #tăng 4 bit địa chỉ cho a1
-	addi $t1, $t1, 1             #tăng biến đếm 
+	addi $a1, $a1, 4            	#tăng 4 bit địa chỉ cho a1
+	addi $t1, $t1, 1             	#tăng biến đếm 
 	j print_array1
 newline:
 	la $a0, string4
@@ -37,9 +37,9 @@ newline:
 	syscall
 	j print_array2
 print_array2:
-	beq $t2, $s2, exit       #nếu $t2 = $s2 thì rẻ nhanh sang exit
-	lb $a0, ($a2)              #đưa địa chỉ của $a2 vào $a0 (byte)
-	li $v0, 1                     #xuất giá trị integer của $a0
+	beq $t2, $s2, exit       	#nếu $t2 = $s2 thì rẻ nhanh sang exit
+	lb $a0, ($a2)              	#đưa địa chỉ của $a2 vào $a0 (byte)
+	li $v0, 1                     	#xuất giá trị integer của $a0
 	syscall
 	la $a0, string3
 	li $v0, 4
@@ -48,7 +48,7 @@ print_array2:
 	addi $t2, $t2, 1
 	j print_array2
 exit:
-	li $v0, 10                $kết thúc chương trình
+	li $v0, 10                	#kết thúc chương trình
 	syscall
 
 
