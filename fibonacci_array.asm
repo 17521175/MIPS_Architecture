ad5dc0c2cdaa8d0f74380beb2dbc570d14fd33a8
TrungNA <84486385+17521175@users.noreply.github.com>
2021-10-25 22:46:38 +0700

    Update fibonacci_array.asm

--- fibonacci_array.asm
+++ fibonacci_array.asm
@@ -4,8 +4,8 @@ XuatFibo: .asciiz "Cac phan tu Fibonacci la : "
 KhongFibo: .asciiz "Khong co gia tri Fibinacci nao"
 Breakline: .asciiz "\n"
 Space : .asciiz " "
+.text
 main:
-	.text
 	li $v0,4			    #syscall xuất chuỗi "NhapFibo"			
 	la $a0,NhapFibo
 	syscall
@@ -22,30 +22,30 @@ main:
 
 fibo:
 
-	beq $t1, $zero, nofibo	#Xét điều kiện nếu số vừa nhập bằng 0 thì out
+	beq $t1, $zero, nofibo			#Xét điều kiện nếu số vừa nhập bằng 0 thì out
 	
-	li $v0,4			    #syscall xuất chuỗi "XuatFibo"	
+	li $v0,4			   	#syscall xuất chuỗi "XuatFibo"	
 	la $a0,XuatFibo
 	syscall
 	
 	addi $s0, $0, 1
-	addi $sp, $sp, -8		#Khởi tạo stack 2 giá trị
+	addi $sp, $sp, -8			#Khởi tạo stack 2 giá trị
 	sw $s0, 0($sp)
 	sw $0, 4($sp)
 	
 Loopfibo:
-	lw $s0, 0($sp)		    #Chuyển số thứ nhất trong chuỗi Fibo ra 
-	lw $s1, 4($sp)		    #Chuyển số thứ hai trong chuỗi Fibo ra
+	lw $s0, 0($sp)		    	#Chuyển số thứ nhất trong chuỗi Fibo ra 
+	lw $s1, 4($sp)		    	#Chuyển số thứ hai trong chuỗi Fibo ra
 	add $a0, $s0, $s1		#Cộng 2 số để ra được số thứ 3 trong chuỗi 
-	sw $s1, 0($sp)		    #Lưu lại số thứ 2 thành số thứ nhất cho lần cộng kế tiếp
-	sw $a0, 4($sp)		    #Lưu lại số thứ 3 thành số thứ hai cho lần cộng kế tiếp
+	sw $s1, 0($sp)		    	#Lưu lại số thứ 2 thành số thứ nhất cho lần cộng kế tiếp
+	sw $a0, 4($sp)		    	#Lưu lại số thứ 3 thành số thứ hai cho lần cộng kế tiếp
 	li $v0, 1
 	syscall
 	li $v0,4		
 	la $a0,Space
 	syscall
-	subi $t1, $t1, 1		    #Trừ số đã nhập đi 1, tạo giới hạn vòng lặp
-	bne $t1, $zero, Loopfibo	#Xét số đã nhập đạt ngưỡng 0 chưa, đạt thì out loop
+	subi $t1, $t1, 1		    	#Trừ số đã nhập đi 1, tạo giới hạn vòng lặp
+	bne $t1, $zero, Loopfibo		#Xét số đã nhập đạt ngưỡng 0 chưa, đạt thì out loop
 	
 	jr $ra
 
